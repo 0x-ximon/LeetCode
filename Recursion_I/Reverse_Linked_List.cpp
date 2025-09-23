f4fcb08b8e9f9d9520016d0674d376fabb59a1fc
Simon Samuel <salviensky@gmail.com>
2025-09-23 08:34:51 +0100

    feat: solved recursive linked list reversal

---
+++ Recursion_I/Reverse_Linked_List.cpp
@@ -0,0 +1,22 @@
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head) { return reverse(head); }
+
+  ListNode* reverse(ListNode* node) {
+    if (!node || !node->next) return node;
+
+    ListNode* head = reverse(node->next);
+    node->next->next = node;
+    node->next = nullptr;
+
+    return head;
+  }
+};
