bf40fb18c8e5cd6f2a08d6f65ed8793396d2e548
Simon Samuel <salviensky@gmail.com>
2025-09-23 16:09:39 +0100

    feat: solved the climb stairs problem

---
+++ Recursion_I/Climbing_Stairs.cpp
@@ -0,0 +1,18 @@
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+ public:
+  int climbStairs(int n) {
+    unordered_map<int, int> m;
+    return climb(n, m);
+  }
+
+  int climb(int n, unordered_map<int, int>& m) {
+    if (n <= 2) return n;
+    if (m.find(n) != m.end()) return m[n];
+
+    m[n] = climb(n - 1, m) + climb(n - 2, m);
+    return m[n];
+  }
+};
